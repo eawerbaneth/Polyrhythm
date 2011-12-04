7d17f0474de92400bceeb921fea535dc39fa486b
eliztowns <townse2@rpi.edu>
2011-12-04 14:52:25 -0500

    tried a bunch of stuff and ended up going back to what it was before

--- Assets/roadscript.cs
+++ Assets/roadscript.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 using System.Collections;
 
 public class roadscript : MonoBehaviour {
-	public Vector3 startpos;
-	public float halflength;
-	public float speed=-.2F;
+	private Vector3 startpos;
+	private float halflength;
+	public float speed_constant;
+	private float speed;
+	
 	// Use this for initialization
 	void Start () {
 		startpos = transform.position;
@@ -13,6 +15,7 @@ public class roadscript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		speed=speed_constant * GameObject.Find("GUI - Bar").GetComponent<GUIScript>().beats_per_second;
 		transform.Translate(speed,0,0);
 		Camera camera = Camera.main;
 		GameObject road2 = GameObject.Find("road2");
